0be3a9c539c9ad2c352d17b9bca994251f48ce3a
Rampagy <alexjsimon32@gmail.com>
2022-12-29 10:05:16 -0600

    Adding overlay plots

--- Tools/commutation_techniques.py
+++ Tools/commutation_techniques.py
@@ -1,5 +1,4 @@
 import math
-import matplotlib.pyplot as plt
 
 
 def degToRad(angDeg):
@@ -31,28 +30,32 @@ def trapezoidal(direct_axis):
         # 300 to 360 degrees
         return (0, 0, 0, 100, 100, 0)
 
-def spwm(direct_axis):
+def spwm(direct_axis, throttle, field_weakening):
+    # throttle (-100 to 100) is the max duty cycle that will be commanded
+    # field weakening (0 to 100) is the percent of field weaking you desire
     # returns tuple of each phases high side mosfet duty cycle (phase a duty, phase b duty, phase c duty)
     # sinsoidal pwm
     
     # control to 90 degrees ahead of the direct axis
     # to go in reverse swap +90 with -90
     # to field weaken change +/-90 to something closer to 0
-    quadrature_axis = direct_axis + 90
-    return (  100*sineLookup(quadrature_axis), 100*sineLookup(quadrature_axis+240), 100*sineLookup(quadrature_axis+120) )
+    quadrature_axis = direct_axis + 90*(100-field_weakening)/100
+    return (  throttle*sineLookup(quadrature_axis), throttle*sineLookup(quadrature_axis+240), throttle*sineLookup(quadrature_axis+120) )
 
-def svpwm(direct_axis):
+def svpwm(direct_axis, throttle, field_weakening):
+    # throttle (-100 to 100) is the max duty cycle that will be commanded
+    # field weakening (0 to 100) is the percent of field weaking you desire
     # returns tuple of each phase's duty cycle (phase a duty, phase b duty, phase c duty)
     # Alternating Reverse Space Vector Modulation
     
-    pwm_period = 100 #0.00002 # in seconds # this is supposed to be in seconds, but by putting 100 seconds it outputs duty cycle in percent
+    pwm_period = throttle # 100 #0.00002 # in seconds # this is supposed to be in seconds, but by putting 100 seconds it outputs duty cycle in percent
     duty_cycle = 1 # in percent
 
 
     # control to 90 degrees ahead of the direct axis
     # to go in reverse swap +90 with -90
     # to field weaken change +/-90 to something closer to 0
-    quadrature_axis = direct_axis + 90
+    quadrature_axis = direct_axis + 90*(100-field_weakening)/100
 
     # this -30 is bugging me, but idk how to get rid of it
     alpha = (quadrature_axis-30) % 60
@@ -88,20 +91,24 @@ def svpwm(direct_axis):
 
 
 if __name__ == '__main__':
+    import matplotlib.pyplot as plt # only needed if this is the main script being run
 
     trap_dutys = []
     spwm_dutys = []
     svpwm_dutys = []
     electrical_phase = []
 
-    # elecitrcal angle is the q axis angle
-    # assume 100% command...
-    for electrical_angle in range(360):
+    # electrical angle is the direct axis angle
+    throttle = 0 # simulate a start from zero throttle ramp
+    for electrical_angle in range(1080):
         electrical_phase += [electrical_angle]
 
         trap_dutys += [trapezoidal(electrical_angle)]
-        spwm_dutys += [spwm(electrical_angle)]
-        svpwm_dutys += [svpwm(electrical_angle)]
+        spwm_dutys += [spwm(electrical_angle, throttle, 0)]
+        svpwm_dutys += [svpwm(electrical_angle, throttle, 0)]
+
+        if throttle < 100:
+            throttle += 0.15
 
 
     spwm_a = []
@@ -120,20 +127,25 @@ if __name__ == '__main__':
         svpwm_b += [b1]
         svpwm_c += [c1]
 
+    # determine the delta voltage (as a percent of VBatt) for each commutation type
+    spwm_dV = [max(a, b, c) - min(a, b, c) for a, b, c in spwm_dutys]
+    svpwm_dV = [max(a, b, c) - min(a, b, c) for a, b, c in svpwm_dutys]
 
     fig, axs = plt.subplots(2)
     fig.suptitle('Commutation techniques')
 
-    axs[0].plot(electrical_phase, spwm_a, electrical_phase, spwm_b, electrical_phase, spwm_c)
-    axs[0].legend(['phase a', 'phase b', 'phase c'])
+    axs[0].plot(electrical_phase, spwm_a, 'g-', electrical_phase, spwm_b, 'b-', electrical_phase, spwm_c, 'r-',
+                electrical_phase, svpwm_a, 'g--', electrical_phase, svpwm_b, 'b--', electrical_phase, svpwm_c, 'r--',)
+    axs[0].legend(['SPWM Phase A', 'SPWM Phase B', 'SPWM Phase C',
+                    'SVPWM Phase A', 'SVPWM Phase B', 'SVPWM Phase C'])
     axs[0].set_ylabel('High Side Transistor Duty Cycle, percent')
     axs[0].set_xlabel('Direct Axis Angle, deg')
     axs[0].set_title('Sinusoidal PWM')
 
-    axs[1].plot(electrical_phase, svpwm_a, electrical_phase, svpwm_b, electrical_phase, svpwm_c)
-    axs[1].legend(['phase a', 'phase b', 'phase c'])
-    axs[1].set_ylabel('High Side Transistor Duty Cycle, percent')
+    axs[1].plot(electrical_phase, spwm_dV, electrical_phase, svpwm_dV)
+    axs[1].legend(['SPWM', 'SVPWM'])
+    axs[1].set_ylabel('Line to Line Voltage (percent of VBatt)')
     axs[1].set_xlabel('Direct Axis Angle, deg')
-    axs[1].set_title('Space Vector PWM')
+    axs[1].set_title('Line to Line Voltage between different commutation techniques')
 
     plt.show()
