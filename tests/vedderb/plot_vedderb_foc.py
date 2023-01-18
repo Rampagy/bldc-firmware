de0a96b9f394b78f7a31d56315a53acb6847cc59
Rampagy <alexjsimon32@gmail.com>
2023-01-17 21:58:41 -0600

    Adding all foc variations

--- tests/vedderb/plot_vedderb_foc.py
+++ tests/vedderb/plot_vedderb_foc.py
@@ -1,28 +1,32 @@
 import matplotlib.pyplot as plt
+import glob
 
 if __name__=='__main__':
-	dutyA = []
-	dutyB = []
-	dutyC = []
-	angles = []
-
-	angle = 0
-	with open('vedderb_foc.csv', 'r') as f:
-		for line in f:
-			dutys = line.strip().split(',')
-			dutyA += [float(dutys[0])]
-			dutyB += [float(dutys[1])]
-			dutyC += [float(dutys[2])]
-
-			angles += [angle]
-			angle += 1
+	svm_methods = glob.glob('svm_*.csv')
+	fig, axs = plt.subplots(len(svm_methods))
+	fig.tight_layout()
 
 
-	fig, axs = plt.subplots(1)
-	axs.plot(angles, dutyA, 'g-', angles, dutyB, 'b-', angles, dutyC, 'r-')
-	axs.legend(['FOC Phase A', 'FOC Phase B', 'FOC Phase C'])
-	axs.set_ylabel('High side transistor duty cycle time (PWMFullDutyCycle = 1000)')
-	axs.set_xlabel('Direct axis angle (deg)')
-	axs.set_title('Ben Vedder FOC duty cycles')
+	for i, svm_method in enumerate(svm_methods):
+		angle = 0
+		dutyA = []
+		dutyB = []
+		dutyC = []
+		angles = []
+		with open(svm_method, 'r') as f:
+			for line in f:
+				dutys = line.strip().split(',')
+				dutyA += [float(dutys[0])]
+				dutyB += [float(dutys[1])]
+				dutyC += [float(dutys[2])]
+
+				angles += [angle]
+				angle += 0.05
+
+		axs[i].plot(angles, dutyA, 'g-', angles, dutyB, 'b-', angles, dutyC, 'r-')
+		axs[i].legend(['Phase A', 'Phase B', 'Phase C'])
+		axs[i].set_ylabel('High side transistor\nduty cycle time')
+		axs[i].set_xlabel('Direct axis angle (deg)')
+		axs[i].set_title(svm_method.split('.')[0] + ' (PWMFullDutyCycle = 1000)')
 
 	plt.show()
--- tests/vedderb/test_vedderb_foc.c
+++ tests/vedderb/test_vedderb_foc.c
@@ -2,232 +2,227 @@
 #include <math.h>
 #include <stdio.h>
 
-#define MAX_AMPLITUDE			(0.866025403784439f)	// max alpha and beta magnitude
-#define ONE_OVER_MAX_AMPLITUDE	(1.0f/MAX_AMPLITUDE)	// reciprocal of max alpha and beta magnitude
+#define SQRT3_BY_2				(0.866025403784439f)	// max alpha and beta magnitude
 #define PI_OVER_180				(0.01745329251f)		// pi/180
 
-#define PI_OVER_3				(1.0471975512f)		// 60 degrees
-#define TWO_PI_OVER_3			(2.0f*PI_OVER_3)	// 120 degrees
-#define THREE_PI_OVER_3			(3.0f*PI_OVER_3)	// 180 degrees
-#define FOUR_PI_OVER_3			(4.0f*PI_OVER_3)	// 240 degrees
-#define FIVE_PI_OVER_3			(5.0f*PI_OVER_3)	// 300 degrees
-#define SIX_PI_OVER_3			(6.0f*PI_OVER_3)	// 360 degrees
-
-// https://opensource.apple.com/source/Libm/Libm-315/Source/ARM/fmodf.c.auto.html
-float fmodf_custom( float x, float y )
+typedef enum
 {
-	union{ float f; uint32_t u; }ux = {x};
-	union{ float f; uint32_t u; }uy = {y};
+	ars_svm_e, // default (previous implementation)
+	null_v0_e, // flat bottom
+	null_v7_e, // flat top
+	v7_odd_v0_even_e, // v7 in [1,3,5] v0 in [2,4,6]
+	v0_odd_v7_even_e, // v0 in [1,3,5] v7 in [2,4,6]
+} commutation_technique_t;
 
-	uint32_t absx = ux.u & 0x7fffffffU;
-	uint32_t absy = uy.u & 0x7fffffffU;
-	if( absx - 1U >= 0x7f7fffffU || absy - 1U >= 0x7f7fffffU )
-	{
-		float fabsx = __builtin_fabsf(x);	
-		float fabsy = __builtin_fabsf(y);
 
-		// deal with NaN
-		if( x != x || y != y )
-			return x + y;
-
-		//x = Inf or y = 0, return Invalid per IEEE-754
-		if( fabsx == __builtin_inff() || 0.0f == y )
-		{
-#ifdef ARMLIBM_SET_FLAGS
-			return required_add_float( __builtin_inff(), -__builtin_inff() );	//set invalid
-#else
-			return __builtin_nan("");
-#endif
-		}
-
-		//handle trivial case
-		if( fabsy == __builtin_inff() || 0.0f == x )
-		{
-#ifdef ARMLIBM_SET_FLAGS
-			required_add_float( fabsx, 0.0f );	// signal underflow (that is, no flag set,
-												// but exception occurs if unmasked) if x is denorm
-#endif
-			return x;
-		}
-	}
- 
-	if( absy >= absx )
-	{
-		if( absy == absx )
-		{
-			ux.u &= 0x80000000;
-			return ux.f;
-		}
-		return x;
-	}
- 
-	//extract exponent, mantissa
-	int32_t expx = absx >> 23;
-	int32_t expy = absy >> 23;
-	int32_t sx = absx & 0x007fffff;			// x significand
-	int32_t sy = absy & 0x007fffff;			// y significand
-	if( 0 == expx )		//denormal x
-	{
-		//calculate shift to move leading bit to exponents field
-		uint32_t shift = __builtin_clzl( absx ) - (8*sizeof(long) - 24);
-		sx <<= shift;	//do the shift
-		expx = 1-shift;	//adjust the biased exponent accordingly, -1 here for the implicit shift to make implicit denorm leading bit explicit
-	}
-	if( 0 == expy )		//denormal y
-	{
-		//calculate shift to move leading bit to exponents field
-		uint32_t shift = __builtin_clzl( absy ) - (8*sizeof(long) - 24);
-		sy <<= shift;	//do the shift
-		expy = 1-shift;	//adjust the biased exponent accordingly, -1 here for the implicit shift to make implicit denorm leading bit explicit
-	}
-	//make implicit bits explicit
-	sx |= 0x00800000;
-	sy |= 0x00800000;
-	
-		
-	// The idea here is to iterate until the exponent of x is the same as y
-	// Calculate the number of bits we can safely shave off before we reach parity
-	int32_t idiff = expx - expy;	
-	int32_t shift = 0;
-	int32_t mask;	
-
-	//since idiff is always >= 0 here, it is safe to enter
-	//We always shift by shift+1 here, so in the first iteration, the worst that can happen
-	do
-	{
-		// move the leading bit of x to the 23rd bit
-		sx <<= shift;
-
-		//Keep track that we did that
-		idiff += ~shift;			// idiff -= shift + 1, +1 for the shift below
-	
-		//The two values both have the 23rd bit set as the leading bit  (24 bit unsigned number)
-		//subtract one from the other. This gives us a signed 23 bit number in the range { -0x00ffffff ... 0x00ffffff }
-		sx -= sy;		
-
-		//record the sign
-		mask = sx >> 31;
-
-		//shift x left 1 to restore a 24 bit quantity
-		sx += sx;		//this is potentially 1 shift too far, which we patch up later
-
-		//if negative, we add back sy to restore to postiveness. This is the same as x - y + 0.5y = - 0.5y 
-		// instead of x-y. We've effectively hoisted the subtract that would have appeared in the next loop
-		// iteration here, and saved a test+branch in exchange for a shift and and. (The add happens either way.)
-		sx += sy & mask;	
-
-		//figure out how far we need to shift sx to get the leading bit into the 23rd position
-		shift = __builtin_clzl( sx ) - (8*sizeof(long) - 24);
-	}
-	while( idiff >= shift && sx != 0);
-	
-	//We may have gone too far 
-	if( idiff < 0 )	
-	{
-		//If so, rewind a little.
-		// if sx - sy succeeded, it was the right thing to do, the only thing we did wrong was the shift
-		// if sx - sy yielded a negative number, then we shouldn't have done that either
-		sx += sy & mask;
-		sx >>= 1;
-//debug code to make sure we didn't do something dumb here
-		idiff = 0;
-	}
-
-	//We may have some bits left over to shift in. 
-	sx <<= idiff;
-	
-//convert back to float
-	if( 0 == sx )
-	{
-		ux.u &= 0x80000000;
-		return ux.f;
-	}
-			
-	//figure out how far we need to shift in order to move leading bit into exponent field
-	shift = __builtin_clzl( sx ) - (8*sizeof(long) - 24);
-	sx <<= shift;				// move leading bit into exponent field
-	expy -= shift;				// account for the shift in the exponent
-	sx &= 0x007fffff;			// remove leading bit
-	sx |= ux.u & 0x80000000;	//or in sign
-	if( expy > 0 )
-	{
-		ux.u = sx | (expy << 23);
-		return ux.f;
-	}
-	
-	//denormal
-	expy += 126;
-	ux.u = sx | (expy << 23);
-	return ux.f * 0x1.0p-126f;
-}
+const uint8_t sector_LUT[6] = {6u, 2u, 1u, 4u, 5u, 3u};
 
 
 void foc_svm(float alpha, float beta, uint32_t PWMFullDutyCycle,
-				uint32_t* tAout, uint32_t* tBout, uint32_t* tCout, uint32_t *svm_sector) {
-	/* Null = V0 SVPWM (reduces switching losses by up to 33%) */
+				uint32_t* tAout, uint32_t* tBout, uint32_t* tCout, 
+				uint32_t *svm_sector, commutation_technique_t comm_tech) {
+	uint8_t sector;
+	uint32_t tA, tB, tC, N;	// PWM timings
+	float T0, T1, T2, a, b, c, temp = 0.0f;
 
-	uint32_t sector;
-	float angle, T1, T2, magnitude, temp, a;
-	uint32_t tA, tB, tC;	// PWM timings
+	a = SQRT3_BY_2*alpha - 0.5f*beta;
+	b = beta;
 
-	// future improvements could use onboard dsp for this
-	// https://www.keil.com/pack/doc/CMSIS/DSP/html/group__atan2.html#gaa7fbd2bb45df78860dd3a0293391096f
-	// bound from 0 to 2*pi
-	angle = atan2f(beta, alpha); // from math.h
+	temp = (float)PWMFullDutyCycle/SQRT3_BY_2;
 
-	// convert from (-pi, pi) to (0, 2*pi) via branchless programming
-	angle = angle + (angle < 0)*SIX_PI_OVER_3;
+	a *= temp;
+	b *= temp;
+	c = -1.0f*(a+b); // kirchhoff current law (current in = current out)
 
-	// future improvements could use onboard dsp for this
-	// https://www.keil.com/pack/doc/CMSIS/DSP/html/group__Euclidean.html#ga838f0a157454dfd95996691309c3828e
-	magnitude = hypotf(alpha, beta); // from math.h
-	a = fmodf(angle, PI_OVER_3); // from math.h
+	N = (((int32_t)a)>=0) + 2u*(((int32_t)b) >= 0) + 4u*(((int32_t)c) >= 0);
+	sector = sector_LUT[N-1];
 
-	// temporary variable for intermediate math
-	temp = (float)PWMFullDutyCycle * magnitude;
+	switch(sector) {
+	case 1: {
+		T1 = a;
+		T2 = b;
+		T0 = (float)PWMFullDutyCycle - T1 - T2;
 
-	// future improvements could use onboard dsp for this (or lookup table)
-	// https://www.keil.com/pack/doc/CMSIS/DSP/html/group__sin.html#gae164899c4a3fc0e946dc5d55555fe541
-	T1 = temp * sinf(PI_OVER_3 - a) * ONE_OVER_MAX_AMPLITUDE; // from math.h
-	T2 = temp * sinf(a) * ONE_OVER_MAX_AMPLITUDE; // from math.h
+		switch (comm_tech) {
+		case ars_svm_e:
+		default:
+			tA = (uint32_t)(T1+T2+0.5f*T0);
+			tB = (uint32_t)(T2+0.5f*T0);
+			tC = (uint32_t)(0.5f*T0);
+			break;
 
+		case v0_odd_v7_even_e:
+		case null_v0_e:
+			tA = (uint32_t)(T1+T2);
+			tB = (uint32_t)(T2);
+			tC = (uint32_t)(0);
+			break;
 
-	if (angle < PI_OVER_3) { // sector 1
-		tA = (uint32_t)(T1+T2);
-		tB = (uint32_t)(T2);
-		tC = (uint32_t)(0);
-		sector = 1u;
-	} else if (angle < TWO_PI_OVER_3) { // sector 2
-		tA = (uint32_t)(T1);
-		tB = (uint32_t)(T1+T2);
-		tC = (uint32_t)(0);
-		sector = 2u;
-	} else if (angle < THREE_PI_OVER_3) { // sector 3
-		tA = (uint32_t)(0);
-		tB = (uint32_t)(T1+T2);
-		tC = (uint32_t)(T2);
-		sector = 3u;
-	} else if (angle < FOUR_PI_OVER_3) { // sector 4
-		tA = (uint32_t)(0);
-		tB = (uint32_t)(T1);
-		tC = (uint32_t)(T1+T2);
-		sector = 4u;
-	} else if (angle < FIVE_PI_OVER_3) { // sector 5
-		tA = (uint32_t)(T2);
-		tB = (uint32_t)(0);
-		tC = (uint32_t)(T1+T2);
-		sector = 5u;
-	} else { // sector 6
-		tA = (uint32_t)(T1+T2);
-		tB = (uint32_t)(0);
-		tC = (uint32_t)(T1);
-		sector = 6u;
+		case v7_odd_v0_even_e:
+		case null_v7_e:
+			tA = (uint32_t)(PWMFullDutyCycle);
+			tB = (uint32_t)(T0+T2);
+			tC = (uint32_t)(T0);
+			break;
+		}
+		break;
+	}
+
+	case 2: {
+		T1 = -c;
+		T2 = -a;
+		T0 = (float)PWMFullDutyCycle - T1 - T2;
+
+		switch (comm_tech) {
+		case ars_svm_e:
+		default:
+			tA = (uint32_t)(T1+0.5f*T0);
+			tB = (uint32_t)(T1+T2+0.5f*T0);
+			tC = (uint32_t)(0.5f*T0);
+			break;
+
+		case v7_odd_v0_even_e:
+		case null_v0_e:
+			tA = (uint32_t)(T1);
+			tB = (uint32_t)(T1+T2);
+			tC = (uint32_t)(0);
+			break;
+
+		case v0_odd_v7_even_e:
+		case null_v7_e:
+			tA = (uint32_t)(T0+T1);
+			tB = (uint32_t)(PWMFullDutyCycle);
+			tC = (uint32_t)(T0);
+			break;
+		}
+		break;
+	}
+
+	case 3: {
+		T1 = b;
+		T2 = c;
+		T0 = (float)PWMFullDutyCycle - T1 - T2;
+
+		switch (comm_tech) {
+		case ars_svm_e:
+		default:
+			tA = (uint32_t)(0.5f*T0);
+			tB = (uint32_t)(T1+T2+0.5f*T0);
+			tC = (uint32_t)(T2+0.5f*T0);
+			break;
+
+		case v0_odd_v7_even_e:
+		case null_v0_e:
+			tA = (uint32_t)(0);
+			tB = (uint32_t)(T1+T2);
+			tC = (uint32_t)(T2);
+			break;
+
+		case v7_odd_v0_even_e:
+		case null_v7_e:
+			tA = (uint32_t)(T0);
+			tB = (uint32_t)(PWMFullDutyCycle);
+			tC = (uint32_t)(T0+T2);
+			break;
+		}
+		break;
+	}
+
+	case 4: {
+		T1 = -a;
+		T2 = -b;
+		T0 = (float)PWMFullDutyCycle - T1 - T2;
+
+		switch (comm_tech) {
+		case ars_svm_e:
+		default:
+			tA = (uint32_t)(0.5f*T0);
+			tB = (uint32_t)(T1+0.5f*T0);
+			tC = (uint32_t)(T1+T2+0.5f*T0);
+			break;
+
+		case v7_odd_v0_even_e:
+		case null_v0_e:
+			tA = (uint32_t)(0);
+			tB = (uint32_t)(T1);
+			tC = (uint32_t)(T1+T2);
+			break;
+
+		case v0_odd_v7_even_e:
+		case null_v7_e:
+			tA = (uint32_t)(T0);
+			tB = (uint32_t)(T0+T1);
+			tC = (uint32_t)(PWMFullDutyCycle);
+			break;
+		}
+		break;
+	}
+
+	case 5: {
+		T1 = c;
+		T2 = a;
+		T0 = (float)PWMFullDutyCycle - T1 - T2;
+
+		switch (comm_tech) {
+		case ars_svm_e:
+		default:
+			tA = (uint32_t)(T2+0.5f*T0);
+			tB = (uint32_t)(0.5f*T0);
+			tC = (uint32_t)(T1+T2+0.5f*T0);
+			break;
+
+		case v0_odd_v7_even_e:
+		case null_v0_e:
+			tA = (uint32_t)(T2);
+			tB = (uint32_t)(0);
+			tC = (uint32_t)(T1+T2);
+			break;
+
+		case v7_odd_v0_even_e:
+		case null_v7_e:
+			tA = (uint32_t)(T0+T2);
+			tB = (uint32_t)(T0);
+			tC = (uint32_t)(PWMFullDutyCycle);
+			break;
+		}
+		break;
+	}
+
+	case 6: {
+		T1 = -b;
+		T2 = -c;
+		T0 = (float)PWMFullDutyCycle - T1 - T2;
+
+		switch (comm_tech) {
+		case ars_svm_e:
+		default:
+			tA = (uint32_t)(T1+T2+0.5f*T0);
+			tB = (uint32_t)(0.5f*T0);
+			tC = (uint32_t)(T1+0.5f*T0);
+			break;
+
+		case v7_odd_v0_even_e:
+		case null_v0_e:
+			tA = (uint32_t)(T1+T2);
+			tB = (uint32_t)(0);
+			tC = (uint32_t)(T1);
+			break;
+
+		case v0_odd_v7_even_e:
+		case null_v7_e:
+			tA = (uint32_t)(PWMFullDutyCycle);
+			tB = (uint32_t)(T0);
+			tC = (uint32_t)(T0+T1);
+			break;
+		}
+		break;
+	}
 	}
 
 	*tAout = tA;
 	*tBout = tB;
 	*tCout = tC;
-	*svm_sector = sector;
+	*svm_sector = (uint32_t)sector;
 }
 
 
@@ -235,20 +230,40 @@ int main (int argc, char* args[]) {
 	(void)argc;
 	(void)args;
 
-	FILE* stream = fopen("vedderb_foc.csv", "w");
-	for (int i = 0; i < 360; ++i) {
+	FILE* ars_stream = fopen("svm_ars.csv", "w");
+	FILE* null_v0_stream = fopen("svm_null_v0.csv", "w");
+	FILE* null_v7_stream = fopen("svm_null_v7.csv", "w");
+	FILE* v7_odd_v0_even_stream = fopen("svm_v7_odd_v0_even.csv", "w");
+	FILE* v0_odd_v7_even_stream = fopen("svm_v0_odd_v7_even.csv", "w");
+
+	for (float i = 0.0f; i < 360.0f; i += 0.05f) {
 		uint32_t tAout, tBout, tCout, sector = 0u;
 		float alpha, beta = 0.0f;
 
 		// determine the alpha-beta vectors from angle-magnitude
-		beta = MAX_AMPLITUDE*sinf( (float)i*PI_OVER_180 );
-		alpha = MAX_AMPLITUDE*cosf( (float)i*PI_OVER_180 );
+		beta = SQRT3_BY_2*sinf( i*PI_OVER_180 );
+		alpha = SQRT3_BY_2*cosf( i*PI_OVER_180 );
 
 		// magnitude is always 1000 (for this test)
-		foc_svm(alpha, beta, (uint32_t)1000u, &tAout, &tBout, &tCout, &sector );
+		foc_svm(alpha, beta, (uint32_t)1000u, &tAout, &tBout, &tCout, &sector, ars_svm_e);
+		fprintf(ars_stream, "%d,%d,%d,%d\n", (int)tAout, (int)tBout, (int)tCout, (int)sector); // write to file
 
-		// write to file
-		fprintf(stream, "%d,%d,%d\n", tAout, tBout, tCout);
+		foc_svm(alpha, beta, (uint32_t)1000u, &tAout, &tBout, &tCout, &sector, null_v0_e);
+		fprintf(null_v0_stream, "%d,%d,%d,%d\n", (int)tAout, (int)tBout, (int)tCout, (int)sector); // write to file
+
+		foc_svm(alpha, beta, (uint32_t)1000u, &tAout, &tBout, &tCout, &sector, null_v7_e);
+		fprintf(null_v7_stream, "%d,%d,%d,%d\n", (int)tAout, (int)tBout, (int)tCout, (int)sector); // write to file
+
+		foc_svm(alpha, beta, (uint32_t)1000u, &tAout, &tBout, &tCout, &sector, v7_odd_v0_even_e);
+		fprintf(v7_odd_v0_even_stream, "%d,%d,%d,%d\n", (int)tAout, (int)tBout, (int)tCout, (int)sector); // write to file
+
+		foc_svm(alpha, beta, (uint32_t)1000u, &tAout, &tBout, &tCout, &sector, v0_odd_v7_even_e);
+		fprintf(v0_odd_v7_even_stream, "%d,%d,%d,%d\n", (int)tAout, (int)tBout, (int)tCout, (int)sector); // write to file
 	}
-	(void)fclose( stream );
+
+	(void)fclose( ars_stream );
+	(void)fclose( null_v0_stream );
+	(void)fclose( null_v7_stream );
+	(void)fclose( v7_odd_v0_even_stream );
+	(void)fclose( v0_odd_v7_even_stream );
 }
