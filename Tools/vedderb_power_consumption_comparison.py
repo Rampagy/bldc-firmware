73541bb4d8e0ec4501e0d6bdd667e0dd3b0d4dc2
Rampagy <alexjsimon32@gmail.com>
2023-02-14 22:37:35 -0600

    Updatin script

--- Tools/vedderb_power_consumption_comparison.py
+++ Tools/vedderb_power_consumption_comparison.py
@@ -28,21 +28,28 @@ if __name__=='__main__':
         df = pd.read_csv(os.path.join(FILE_PATH, f), names=HEADERS, delimiter=';', index_col=False)
 
         # get average watts/rpm in the last 2 sec of each test
-        ave_time = df[df['Time'] > 18]
-        ave_power = (ave_time['RPM']/ave_time['Power']).mean()
-        print('{:>16s} :: {:5.1f} RPM/watt'.format(test_name, ave_power))
+        power_time = df[df['Time'] > 18]
+        ave_power = (power_time['RPM']/power_time['Power']).mean()
 
-        axes[i].plot(df['Time'], df['Power'])
+        # get final average temperature
+        temp_time = df[df['Time'] > 19.5]
+        final_temp = temp_time['MOSFET Temp 1'].mean()
+
+        print('{:>16s} :: {:5.1f} RPM/watt ({:4.1f} °C)'.format(test_name, ave_power, final_temp))
+
+        axes[i].plot(df['Time'], df['MOSFET Temp 1'], df['Time'], df['MOSFET Temp 2'], df['Time'], df['MOSFET Temp 3'])
         axes[i].set_title(test_name)
         axes[i].set_xlabel('Time, s')
-        axes[i].set_ylabel('Power, watts')
+        axes[i].set_ylabel('FET Temperature, C')
+        axes[i].grid()
+        axes[i].set_ylim(bottom=18, top=24.5)
 
 
 
     plt.subplots_adjust(left=0.1,
-                    bottom=0.1,
+                    bottom=0.05,
                     right=0.9,
-                    top=0.9,
+                    top=0.95,
                     wspace=0.5,
                     hspace=0.5)
     plt.show()
